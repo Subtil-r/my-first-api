730bc6843b07e9f38602b36e7606d838b1c9eab8
Subtil-r <subtilrenato@gmail.com>
2021-05-26 15:50:45 +0200

    buttons working & listing contents

--- script.js
+++ script.js
@@ -1,32 +1,74 @@
-
+const resContainer = document.querySelector('.res_container')
+const byList = document.querySelector('#btnList')
+const byName = document.querySelector('#btnName')
+const byIng = document.querySelector('#btnIngredient')
 
 //list all recipes with input name
-fetch(' https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-  .then(response => response.json())
-  .then((data) => {
-    console.log(data);
-    //loop to show all array elements
-    for(var i = 0; i < data.drinks.length ; i++){
-    console.log(data.drinks[i].strDrink);}
-  })
+const lstName = (listName) => {
+  resContainer.innerHTML = "" //cleans the previous result
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${listName}`)
+    .then(response => response.json())
+    .then((data) => {
+      //loop to show all array elements
+       for(var i = 0; i < data.drinks.length ; i++){
+        let drinkName = `<h3>${data.drinks[i].strDrink}<\h3>`
+        resContainer.insertAdjacentHTML('beforeend', drinkName)
+      } 
+    })
+
+}
+
+byName.addEventListener('click', (event) => {
+  let input = document.querySelector('#iptSearch')
+  lstName(input.value)
+})
+
+
 
   //list all recipes by input letter
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=c')
-.then(response => response.json())
-.then((data) => {
-  console.log(data);
-  //loop to show all array elements
-  for(var i = 0; i < data.drinks.length ; i++){
-  console.log(data.drinks[i].strDrink);}
-})
+  const lstLetter = (listLetter) => {
+    resContainer.innerHTML = "" //cleans the previous result
+  
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${listLetter}`)
+      .then(response => response.json())
+      .then((data) => {
+        //loop to show all array elements
+         for(var i = 0; i < data.drinks.length ; i++){
+          let drinkLetter = `<h3>${data.drinks[i].strDrink}<\h3>`
+          resContainer.insertAdjacentHTML('beforeend', drinkLetter)
+        } 
+      })
+  
+  }
+  
+  byList.addEventListener('click', (event) => {
+    let input = document.querySelector('#iptSearch')
+    lstLetter(input.value)
+  })
+
 
 //list all recipes by input ingredient
-fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin')
-.then(response => response.json())
-.then((data) => {
-  console.log(data);
-  //loop to show all array elements
-  for(var i = 0; i < data.drinks.length ; i++){
-    console.log(data.drinks[i].strDrink);}
+const lstIngredient = (listIngredient) => {
+  resContainer.innerHTML = "" //cleans the previous result
+
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${listIngredient}`)
+    .then(response => response.json())
+    .then((data) => {
+      //loop to show all array elements
+       for(var i = 0; i < data.drinks.length ; i++){
+        let drinkIngredient = `<h3>${data.drinks[i].strDrink}<\h3>`
+        resContainer.insertAdjacentHTML('beforeend', drinkIngredient)
+      } 
+    })
+
+}
+
+byIng.addEventListener('click', (event) => {
+  let input = document.querySelector('#iptSearch')
+  lstIngredient(input.value)
 })
+
+
+
 
